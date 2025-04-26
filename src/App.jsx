6d4591bc5fb790a50a5292bd2acec3b78296097a
Musraf <mussraffro45@gmail.com>
2025-04-26 23:49:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg"
 import MovieCard from './MovieCard'
 
 //84b08516
-const API_URL ='http://www.omdbapi.com/?apikey=84b08516'
+const API_URL ='https://www.omdbapi.com/?apikey=84b08516'
 
 const  movie = 
   {
